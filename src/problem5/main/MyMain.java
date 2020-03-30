ecebe0f5ad5fe32cf6c5d5dfa9d1e87d8fcdac78
PranshiAgarwal <62603183+PranshiAgarwal@users.noreply.github.com>
2020-03-31 00:09:22 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,29 @@
  */
 package problem5.main;
 //executable class
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+import java.util.Random;
 public class MyMain {
+  public static void main(String[] args) {
+        Random rand = new Random();
+        MyCircularQueue queue = new MyCircularQueue();
+        System.out.println("Adding Students :\n---------------------------");
+        queue.enqueue(new Student(1, "virus", 2, rand.nextInt(2) + 1));
+        queue.display();
+        queue.enqueue(new Student(2, "ninja", 4, rand.nextInt(2) + 1));
+        queue.display();
+        queue.enqueue(new Student(3, "Magneto", 3, rand.nextInt(2) + 1));
+        queue.display();
+        queue.enqueue(new Student(4, "Hulk", 5, rand.nextInt(2) + 1));
+        queue.display();
+        queue.enqueue(new Student(5, "Professor", 0, rand.nextInt(2) + 1));
+        queue.display(); System.out.println("Processing Elements :\n---------------------------");
+        while (queue.processStudents() != false) {
+        }
+        queue.display();
+    }
 }
+          
+    
+
